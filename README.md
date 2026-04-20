e3f09e258e605abf53c5daeae79cfcc88257d2d8
PekaDur <reljinpetar@gmail.com>
2026-04-21 00:06:50 +0200

    drugi commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # PrototipKorisnickihInterfejsa
 
-- Angular 20
+- Angular 21
 - Bootstrap 5
 - FontsAwesome 7
 - Axios HTTP 
---
+++ src/app/home/home.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import axios from "axios";
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+  protected webData = signal('')
+
+  constructor() {
+    axios.get('./data.json')
+    .then(rsp => this.webData.set(JSON.stringify(rsp.data)))
+  }
+}
