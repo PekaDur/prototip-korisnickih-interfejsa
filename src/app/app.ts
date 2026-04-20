807d557d880f51b0654b6c63aaba0046b2abf7e4
PekaDur <reljinpetar@gmail.com>
2026-04-20 17:19:24 +0200

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('prototip-korisnickih-interfejsa');
+  protected readonly title = signal('petar');
 }
